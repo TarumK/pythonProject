d7b7c0626861500e0f023b5eef1f4c37a01bbb57
slkom2 <slkom2@gmail.com>
2022-11-01 14:59:18 +0300

    Добавлено меню

--- dproject/superapp/views.py
+++ dproject/superapp/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse('Index')
 
-def base(request):
-    return render(request, 'base.html')
 
-def about(request):
+def base(request):
+    some_list = [2 ** x for x in range(50)]
+    some_string = ' '.join(map(str, some_list))
+    return render(request, 'base.html', {'name': some_string})
+
+
+def about():
     return HttpResponse('About')
