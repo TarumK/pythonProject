c4f78e121efe9ccc39d42e6240623c02f7136098
slkom2 <slkom2@gmail.com>
2022-11-01 16:16:14 +0300

    Добавлено поле о наличии образования

--- dproject/superapp/views.py
+++ dproject/superapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .forms import UserForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
-    # return HttpResponse('Index')
+    if request.method == "POST":
+        name = request.POST.get("name") # получить значение поля Имя
+        age = request.POST.get("age")  # получить значение поля Возраст
+        output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст - {1} </hЗ>".format(name, age)
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request, "index.html", {"form": userform})
 
 
 def base(request):
