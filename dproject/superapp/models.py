bc47992c5b917f3913af125e64c2b238e9af85f5
TarumK <kyabishev72@gmail.com>
2022-11-04 13:24:57 +0300

    1234

--- dproject/superapp/models.py
+++ dproject/superapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Town(models.Model):
-    name = models.CharField(max_length=50)
-    # name = models.ForeignKey(Person, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return (self.name)
+# class Town(models.Model):
+#     name = models.CharField(max_length=50)
+#     # name = models.ForeignKey(Person, on_delete = models.CASCADE)
+#
+#     def __str__(self):
+#         return (self.name)
 
 class Person(models.Model):
-    town = models.ForeignKey(Town, on_delete=models.CASCADE)
+    # town = models.ForeignKey(Town, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     objects = models.Manager()
